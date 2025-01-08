efb36097f6ae24f2c20ac166ed13a1443092a211
DidierPereira <didier@jobchain.com>
2025-01-08 14:31:05 -0500

    (fix/details)

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const DetailScreen = () => {
+const DetailsScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
@@ -9,4 +9,4 @@ const DetailScreen = () => {
   );
 };
 
-export default DetailScreen;
+export default DetailsScreen;
